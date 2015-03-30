b54d087a1e2cc50d4f3bde241e85ac47a1745301
wolfram74 <phaugen@gmail.com>
2015-03-30 10:16:57 -0500

    implemented CV view

--- app/assets/javascripts/ajax_handler.js
+++ app/assets/javascripts/ajax_handler.js
@@ -8,24 +8,46 @@ function bindEvents(){
 
 function headerGrab(){
   event.preventDefault();
+  if (this.href.indexOf("projects")>-1){
+    grabProjects(this.href)
+  } else if (this.href.indexOf("cv")>-1){
+    grabCV(this.href)
+  };
+};
+
+function grabProjects(url){
   $.ajax({
     type:"GET",
-    url: this.href
-  }).done(loadPage)
+    url: url
+  }).done(loadProjects)
 }
 
-function loadPage(objects){
+function grabCV(url){
+  loadCV()
+}
+
+
+
+function loadProjects(objects){
   var source = $("#projectsIndex").html();
   var template = Handlebars.compile(source);
   var context = {projects: objects}
-  // var tc= 
-  // console.log(objects)
   var rendered = template(context)
-  // $("#stage").empty()
-  $("#startpoint").hide()
-  $("#stage").append(rendered)
+  viewHandler(rendered)
 }
 
+function loadCV(){
+  var source = $("#cvIndex").html();
+  var template = Handlebars.compile(source);
+  var context = {}
+  var rendered = template(context)
+  viewHandler(rendered)  
+}
+
+function viewHandler(newSection){
+  $("#stage").children().hide()
+  $("#stage").append(newSection)
+}
 function shout(){
   console.log("EVERYTHING IS HAPPENING?!S")
 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326224138) do
+ActiveRecord::Schema.define(version: 20150330141737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collaborators", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "url"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "title"
