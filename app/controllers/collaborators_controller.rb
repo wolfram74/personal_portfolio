30e855703620964318968e7d296fe2bbb978b2ca
wolfram74 <phaugen@gmail.com>
2015-04-30 09:21:04 -0700

    implemented views for collaborators editing

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,6 +1,44 @@
 class CollaboratorsController < ApplicationController
   def index
-    puts "hit route"
-    render json: Collaborator.all
+    @daily_pass = Lock.find_by(property: "admin_pass").value
+    if params["admin_pass"] != @daily_pass
+      render json: Collaborator.all
+    else
+      @collaborators = Collaborator.all
+      @new_collaborator = Collaborator.new()
+    end
+  end
+  def show
+    @daily_pass = Lock.find_by(property: "admin_pass").value
+    if params["admin_pass"] != @daily_pass
+      redirect_to("/")
+    else
+      @collaborator = Collaborator.find(params["id"])
+    end
+  end
+
+  def create
+    Collaborator.create(collab_params)
+    redirect_to("/")
+  end
+
+  def update
+    Collaborator.find(params["id"]).update_attributes(collab_params)
+    redirect_to("/")
+  end
+
+  def destroy
+    Collaborator.find(params["id"]).destroy
+    redirect_to("/")
+  end
+
+  private
+
+  def collab_params
+      params.require(:collaborator).permit(
+        :name, 
+        :description, 
+        :email,
+        :url)
   end
 end
